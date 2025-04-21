405bad82238dc094a7c2d9540a282d894c1cfa3c
unknown <JinHak-BBulSo>
2025-04-21 17:48:46 +0900

    Editor Build Test Code Add

---
+++ Assets/Editor/TestBuilder.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestBuilder : MonoBehaviour
+{
+    [MenuItem("Build/BuildTest_Window")]
+    public static void BuildTest()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        List<string> scenes = new List<string>();
+        foreach(var scene in EditorBuildSettings.scenes)
+        {
+            if(!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+
+        buildPlayerOptions.scenes = scenes.ToArray();
+        buildPlayerOptions.locationPathName = "Build/Test_Window.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if(summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded");
+        }
+
+        if(summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build fail");
+        }
+    }
+}
